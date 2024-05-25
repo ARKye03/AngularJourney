4ece34571659e03bee12ba9743f3b55abf66bcd9
ARKye03 <rafa2003sanchez@gmail.com>
2024-05-24 22:52:44 -0400

    INIT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { elementAt } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  template: `
+  <h1>This is my first app in angular</h1>
+  <p>My name is {{user}} </p>
+
+  `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'Angular Journey'
+  user = 'Nixarkye'
 }
